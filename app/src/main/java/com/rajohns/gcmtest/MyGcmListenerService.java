3cb5151f3f2e0170a81677d517b29f3a5ae814b4
Adam Johns <robert.adam.johns@gmail.com>
2016-04-06 06:59:14 -0500

    show a notification when a notification is received from gcm

--- app/src/main/java/com/rajohns/gcmtest/MyGcmListenerService.java
+++ app/src/main/java/com/rajohns/gcmtest/MyGcmListenerService.java
@@ -1,6 +1,44 @@
 package com.rajohns.gcmtest;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
 import com.google.android.gms.gcm.GcmListenerService;
 
 public class MyGcmListenerService extends GcmListenerService {
+
+    @Override
+    public void onMessageReceived(String from, Bundle data) {
+        String message = data.getString("message");
+        Log.d("tagzzz", "From: " + from);
+        Log.d("tagzzz", "Message: " + message);
+        showNotification(message);
+    }
+
+    private void showNotification(String message) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+            .setSmallIcon(android.R.drawable.btn_dialog)
+            .setContentTitle("GCM Message")
+            .setContentText(message)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
 }
